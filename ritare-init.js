c76ae320a98ab0b4a332c15565b9b7cb992fff2d
iscrooble <68666403+iscrooble@users.noreply.github.com>
2021-02-04 14:36:53 -0500

    Update ritare-init.js

--- ritare-init.js
+++ ritare-init.js
@@ -4,7 +4,7 @@ Ritare.start({
         Ritare.canvas.toBlob((blob)=>{
             let formData = new FormData()
             formData.append("drawing",blob,"blob.png")
-            fetch("https://www.xn--david-9u04d.ws/submission",{method:"POST",body:formData})
+            fetch("https://www.xn--david-9u04d.ws/submission",{method:"POST", mode: 'no-cors', body:formData})
                 .then(response => {
                     if (!response.ok) {
                         throw new Error('Network response was not ok');
