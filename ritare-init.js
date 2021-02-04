6d3f3871d72714fe71741f2ba9ea19e53db60d52
iscrooble <68666403+iscrooble@users.noreply.github.com>
2021-02-04 13:16:50 -0500

    Update ritare-init.js

--- ritare-init.js
+++ ritare-init.js
@@ -1,43 +1,23 @@
-#ritare-canvas {
-	border-style:solid;
-	border-color:#333;
-	border-width:1px;
-	border-radius: 3px;
-	float:left;
-	clear:none;
-	margin-bottom:4px;
-}
-
-#selectors {
-	float:left;
-}
-
-#selectors input {
-	color:white;
-}
-
-#toolselection {
-	margin:2px;
-}
-
-#widthselect {
-	width:30px;
-	margin:2px;
-}
-
-#ritare span {
-	float:left;
-	margin:3px;
-	margin-left:3px;
-	font-family:sans-serif;
-}
-
-#picker {
-	margin:2px;
-	margin-left:3px;
-}
-
-#ritare-finished {
-	margin:3px;
-	float:right;
-}
+Ritare.start({
+    parentel: "painter",
+    onFinish: function(e) {
+        Ritare.canvas.toBlob((blob)=>{
+            let formData = new FormData()
+            formData.append("drawing",blob,"blob.png")
+            fetch("https://www.xn--david-9u04d.ws/submission",{method:"POST",body:formData})
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    location.href = "/guestbook_entries.html"
+                    return response;
+                })
+                .catch(error => {
+                    console.error('There has been a problem with your fetch operation:', error);
+                });
+        }, "image/png")
+        
+    },
+    width:512,
+    height:512
+});
